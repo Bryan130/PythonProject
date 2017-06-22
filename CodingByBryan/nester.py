04f0950a3bf60bbece520e608ad187ec32133cbd
haitao.lan@longsys.com <haitao.lan@longsys.com>
2017-06-22 14:17:24 +0800

    add nester.py

---
+++ CodingByBryan/nester.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Date:2017-06-21 19:52:12
+# @Author:Bryan (bryan.lan@vengasz.com)
+# @Link:http://.com
+
+def print_lol(the_list):
+    """这是“nester.py”模块，提供了一个名为
+    print_lol()的函数，这个函数的作用是打印列表，
+    其中有可能包含（也可能不包含）嵌套列表。"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
---
+++ CodingByBryan/test/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*-
+# @Date:2017-06-21 19:52:12
+# @Author:Bryan (bryan.lan@vengasz.com)
+
+a = 1
+a = 2
+a = 3
+a = 4
+print(a)

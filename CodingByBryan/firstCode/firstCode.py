69339468899ac51d0fe355edf50b56a1ed5e219d
haitao.lan@longsys.com <haitao.lan@longsys.com>
2017-06-23 15:22:05 +0800

    version 1.1.0 add indentation

--- CodingByBryan/firstCode/firstCode.py
+++ CodingByBryan/firstCode/firstCode.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # @Date:2017-06-21 19:52:12
-# @Author:Bryan.Lan (bryan.lan@vengasz.com)
+# @Author:Bryan (bryan.lan@vengasz.com)
 # @Link:https://github.com/Bryan130
 
 
 def print_lol(the_list, level):
-    """这是“nester.py”模块，提供了一个名为
+    """这是“firstCode.py”模块，提供了一个名为
     print_lol()的函数，这个函数的作用是打印列表，
     其中有可能包含（也可能不包含）嵌套列表。
     第二个参数（名为level）用例再遇到嵌套列表是插入制表符"""
@@ -17,7 +17,3 @@ def print_lol(the_list, level):
             for tab_stop in range(level):
                 print("\t", end='')
             print(each_item)
-
-
-cast = ['Palin', 'Cleese', ['Idle', ['Jones', 111]]]
-print_lol(cast, True)
---
+++ CodingByBryan/firstCode/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Date:2017-06-21 19:57:34
+# @Author:Bryan (bryan.lan@vengasz.com)
+# @Link:https://github.com/Bryan130
+
+from distutils.core import setup
+
+setup(
+    name='nester',
+    version='1.1.0',
+    py_modules=['firstCode'],
+    author='Bryan.lan',
+    author_email='bryan.lan@vengasz.com',
+    url='https://github.com/Bryan130',
+    description='A simple printer of nested lists')

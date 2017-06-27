983e54e6c9f62daecbaac91d6cba43c3e2069806
haitao.lan@longsys.com <haitao.lan@longsys.com>
2017-06-27 16:50:41 +0800

    兼容上个版本，增加indent参数，可以不缩进

--- CodingByBryan/firstCode/firstCode.py
+++ CodingByBryan/firstCode/firstCode.py
@@ -5,15 +5,16 @@
 # @Link:https://github.com/Bryan130
 
 
-def print_lol(the_list, level=0):
+def print_lol(the_list, indent=False, level=0):
     """这是“nester.py”模块，提供了一个名为
     print_lol()的函数，这个函数的作用是打印列表，
     其中有可能包含（也可能不包含）嵌套列表。
     第二个参数（名为level）用例再遇到嵌套列表是插入制表符"""
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, level+1)
+            print_lol(each_item, indent, level + 1)
         else:
-            for tab_stop in range(level):
-                print("\t", end='')
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
             print(each_item)
